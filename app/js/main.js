bc096c00faa842757062f9a678e09216aa6a747b
adamisscenery <kaurimdu@gmail.com>
2020-09-11 07:03:00 +0600

    added half-page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,14 @@
 $(function(){
 
+    $(".product__sell-rate-star").rateYo({
+        rating: 4.5,
+        starWidth: "14px",
+        readOnly: true,
+    });
+
+    var mixer = mixitup('.sale__wrapper');
+
+
 
     
 });
